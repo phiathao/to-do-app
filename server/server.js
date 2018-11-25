4c75d971fc2938f89c3dfb3c7434f1c48a6d23ca
phiathao <phiat11@gmail.com>
2018-11-25 16:18:05 -0600

    server add db

--- server/server.js
+++ server/server.js
@@ -5,6 +5,26 @@ const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 
+//db stuff
+const pg = require('pg');
+
+const Pool = pg.Pool;
+const pool = new Pool ({
+    database: 'todoDB',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000
+})
+
+pool.on('connect', ()=>{
+    console.log('connected to DB');
+});
+  
+pool.on('error', (error)=>{
+    console.log('Error connecting to DB', error);
+});
+// end db stuff
 
 //static
 app.use(bodyParser.urlencoded({extended: true}));
