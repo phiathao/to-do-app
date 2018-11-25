c65a4ac2f7f0a97d4968ba3db70bcc3e59290f52
phiathao <phiat11@gmail.com>
2018-11-25 16:14:02 -0600

    add db

---
+++ todoDB.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "todolist" (
+	"task" VARCHAR(255) NOT NULL,
+	"important" BOOLEAN,
+	"complete" BOOLEAN DEFAULT '0',
+	"id" SERIAL PRIMARY KEY
+);
+
+INSERT INTO "todolist" ("task", "important") VALUES('Wash the dish', '0'), 
+('Wipe the window', '0'),
+('Take out the trash', '0'),
+('Buy a chicken for dinner', '1'),
+('Cook dinner', '0');
