e1fc715520bd527c555974f6f2d2b7ca043e2161
phiathao <phiat11@gmail.com>
2018-11-25 14:11:50 -0600

    setup server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+
+// setup/require
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+
+
+//static
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('server/public'));
+
+
+//starting server
+app.listen(port, ()=>{
+    console.log('listing on port', PORT);
+});
